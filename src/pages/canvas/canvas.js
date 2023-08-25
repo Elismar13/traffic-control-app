7b0a6c5f6ee9192499821ed4b82fcd79bb061363
Elismar13 <elismarsilva.5@outlook.com>
2023-08-25 17:58:41 -0300

    dealing with user custom coordinates

--- src/pages/canvas/canvas.js
+++ src/pages/canvas/canvas.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const imageUpload = document.getElementById('imageUpload');
-
 const MAX_POINTS = 4;
+
 let image;
-let boxes = [];
+let paths = [];
 let points = [];
 let colorPointCount = 0;
 let actualColor = 0;
@@ -13,6 +13,7 @@ let colors = ['#FF6633', '#FF33FF', '#FFFF99', '#00B3E6',
   '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
   '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933'
 ];
+let currentColor = colors[actualColor];
 
 function drawSinglePoint(point) {
   ctx.beginPath();
@@ -25,18 +26,31 @@ function drawPoints() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(image, 0, 0);
 
+  for (const path of paths) {
+    drawPathOutline(path.color, path.points);
+  }
+
   for (const point of points) {
     drawSinglePoint(point);
   }
-
-  for(const box of boxes) {
-    for (const point of box) {
-      drawSinglePoint(point);
-    }
-  }
 }
 
-imageUpload.addEventListener('change', function (event) {
+function drawPathOutline(color, path) {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(path[path.length - 1].x, path[path.length - 1].y);
+
+  for (const point of path) {
+    ctx.lineTo(point.x, point.y);
+  }
+
+  ctx.closePath(); // Close the path to form a coherent contour
+  ctx.stroke();
+  ctx.lineWidth = 1;
+}
+
+function openFile(event) {
   const file = event.target.files[0];
 
   if (file && file.type.startsWith('image/')) {
@@ -52,25 +66,51 @@ imageUpload.addEventListener('change', function (event) {
     };
     reader.readAsDataURL(file);
   }
-});
+}
 
-canvas.addEventListener('click', function (event) {
-  if (colorPointCount >= MAX_POINTS) {
-    if(actualColor >= colors.length)
-      actualColor = 0;
-    colorPointCount = 0;
-    actualColor++;
-    boxes.push(points);
-    points = [];
-  }
-
-  const color = colors[actualColor];
-  const x = event.clientX - canvas.getBoundingClientRect().left;
-  const y = event.clientY - canvas.getBoundingClientRect().top;
+function addPointListener(mouseEvent) {
+  const x = mouseEvent.clientX - canvas.getBoundingClientRect().left;
+  const y = mouseEvent.clientY - canvas.getBoundingClientRect().top;
 
   colorPointCount++;
-  points.push({ x, y, color });
+  points.push({ x, y, color: currentColor });
   drawPoints();
 
-  console.log(`Cord x: ${x}; Cord y: ${y}; Cor: ${color}`);
-});
+  console.log(`Cord x: ${x}; Cord y: ${y}; Cor: ${currentColor}`);
+
+  if (colorPointCount === MAX_POINTS) {
+    organizePoints();
+    createPath();
+    colorPointCount = 0;
+  }
+}
+
+function organizePoints() {
+  // Sort points by angle to create a coherent contour
+  const center = calculateCenter(points);
+  points.sort((a, b) => {
+    const angleA = Math.atan2(a.y - center.y, a.x - center.x);
+    const angleB = Math.atan2(b.y - center.y, b.x - center.x);
+    return angleA - angleB;
+  });
+}
+
+function calculateCenter(points) {
+  let centerX = 0;
+  let centerY = 0;
+
+  for (const point of points) {
+    centerX += point.x;
+    centerY += point.y;
+  }
+
+  return { x: centerX / points.length, y: centerY / points.length };
+}
+
+function createPath() {
+  paths.push({ color: currentColor, points: [...points] });
+  points = [];
+}
+
+imageUpload.addEventListener('change', openFile);
+canvas.addEventListener('click', addPointListener);
